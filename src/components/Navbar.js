490bd4da13a546487e8fc83906041630bc96db2f
Rachid HAMMA ALI <r.hammaali@gmail.com>
2022-04-10 05:49:21 +0000

    Add login btns to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
     return (
         <div>
             <nav className="flex fixed w-full shadow-lg">
-                <div className="flex w-full container mx-auto">
+                <div className="flex w-full lg:container mx-auto">
                     <div className="logo flex items-center">
                         <img className="h-16" src="https://upload.wikimedia.org/wikipedia/commons/2/2c/OneWeb_Logo.png" alt="Logo Image"/>
                     </div>
@@ -35,6 +35,7 @@ const Navbar = () => {
                             <li><a href="#">Products</a></li>
                             <li><a href="#">Services</a></li>
                             <li><a href="#">Contact Us</a></li>
+                            <li className="show-login-btn"><button className="login-button">Login</button><button className="join-button">Join</button></li>
                         </ul>
                         <ul className="nav-links border-l">
                             <li className="ml-2"><button className="login-button">Login</button></li>
