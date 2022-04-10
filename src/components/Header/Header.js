5a088c02a815568eae2636833633019815223dba
Rachid HAMMA ALI <r.hammaali@gmail.com>
2022-04-10 17:36:35 +0000

    Done header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Slider from "./Slider";
+import {ArrowRightAlt} from "@mui/icons-material";
 
 const Header = () => {
     return (
         <header className="w-screen h-screen">
-            <div className="mt-16 pt-4">
+            <div className="h-full">
                 <Slider/>
                 <div id="header-content" className="absolute p-8">
-                    <button className="book-btn w-1/4 border-solid border-stone-50 py-2 lg:py-4 px-4 lg:px-8  bg-blue-600 text-center text-white font-bold text-xl rounded-full shadow-xl uppercase" >Book Now</button>
+                    <p className="text-2xl lg:text-4xl font-bold mb-5 ml-2 text-white ">
+                        For best experiences,<br/>
+                        go with experienced guides
+                    </p>
+                    <button className="book-btn w-full lg:w-1/5 py-2 lg:py-4 px-4 lg:px-8  bg-blue-600 text-center text-white font-500 text-xl rounded-full shadow-xl shadow-inner uppercase" >Book Now <ArrowRightAlt/> </button>
                 </div>
             </div>
         </header>
--- src/components/Header/Slider.js
+++ src/components/Header/Slider.js
@@ -5,29 +5,56 @@ import SSlider from "react-slick";
 import s1 from "../../images/slides/s1.jpg";
 import s2 from "../../images/slides/s2.jpg";
 import s3 from "../../images/slides/s3.jpg";
+import s1phone from "../../images/slides/phone/s1.jpg";
+import s2phone from "../../images/slides/phone/s2.jpg";
+import s3phone from "../../images/slides/phone/s3.jpg";
 
 const Slider = () => {
     return (
         <div id="slider">
-            <SSlider
-                dots={false}
-                infinite={true}
-                speed={3000}
-                slidesToShow={1}
-                slidesToScroll={1}
-                autoplay={true}
-                autoplaySpeed={2500}
-            >
-                <div>
-                    <img src={s1} className="w-full h-full" alt="ed"/>
-                </div>
-                <div>
-                    <img src={s2} className="w-full h-full" alt="ed"/>
-                </div>
-                <div>
-                    <img src={s3} className="w-full h-full" alt="ed"/>
-                </div>
-            </SSlider>
+            <div className="block md:hidden">
+                <SSlider
+                    dots={false}
+                    infinite={true}
+                    speed={3000}
+                    slidesToShow={1}
+                    slidesToScroll={1}
+                    autoplay={true}
+                    autoplaySpeed={2500}
+                >
+                    <div id="phone-slid">
+                        <img src={s1phone} className="w-full h-full" alt="ed"/>
+                    </div>
+                    <div id="phone-slid">
+                        <img src={s2phone} className="w-full h-full" alt="ed"/>
+                    </div>
+                    <div id="phone-slid">
+                        <img src={s3phone} className="w-full h-full" alt="ed"/>
+                    </div>
+                </SSlider>
+            </div>
+
+            <div className="hidden md:block">
+                <SSlider
+                    dots={false}
+                    infinite={true}
+                    speed={3000}
+                    slidesToShow={1}
+                    slidesToScroll={1}
+                    autoplay={true}
+                    autoplaySpeed={2500}
+                >
+                    <div id="web-slid">
+                        <img src={s1} className="w-full h-full" alt="ed"/>
+                    </div>
+                    <div id="web-slid">
+                        <img src={s2} className="w-full h-full" alt="ed"/>
+                    </div>
+                    <div id="web-slid">
+                        <img src={s3} className="w-full h-full" alt="ed"/>
+                    </div>
+                </SSlider>
+            </div>
         </div>
     );
 };
